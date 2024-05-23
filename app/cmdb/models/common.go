52c0f1a69ef07a0d6319686145303abd17bdc663
chuanqidota <chuanqidota@126.com>
2024-05-24 07:21:35 +0800

    field-json

--- app/cmdb/models/common.go
+++ app/cmdb/models/common.go
@@ -8,6 +8,6 @@ import (
 type BaseModel struct {
 	ID        uint           `gorm:"column:id; primary_key; AUTO_INCREMENT" json:"id"`
 	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"column:updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
