bc393ea3dfc934ed33a578c818e9b1ebdffb85fa
dota <76216654+chuanqidota@users.noreply.github.com>
2025-01-12 17:27:06 +0800

    feat(middleware): 优化 CORS 中间件
    - 添加允许凭证的跨域请求支持
    - 使用 http 包的常量替换硬编码的 HTTP 方法

--- pkg/middleware/cores.go
+++ pkg/middleware/cores.go
@@ -1,14 +1,18 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, Authorization")
 
-		if c.Request.Method == "OPTIONS" {
+		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(200)
 			return
 		}
